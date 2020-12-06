3340941ca7de74a8aaa722b29bcf158315d93683
Roy Malka <Roymal1788@gmail.com>
2020-12-06 20:37:53 +0200

    first commit

---
+++ Source/KrazyCarts/KrazyCartsGameMode.h
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "KrazyCartsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AKrazyCartsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AKrazyCartsGameMode();
+};
+
+
+
---
+++ Source/KrazyCarts/KrazyCartsWheelFront.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VehicleWheel.h"
+#include "KrazyCartsWheelFront.generated.h"
+
+UCLASS()
+class UKrazyCartsWheelFront : public UVehicleWheel
+{
+	GENERATED_BODY()
+
+public:
+	UKrazyCartsWheelFront();
+};
+
+
+
